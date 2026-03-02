411fb2e9829b67c74553b33465ece0bc28ee01dd
Ziad \ Lyudmira Segfaultovna <sahraouitarikziad@gmail.com>
2026-03-02 16:06:34 +0100

    my feedback

--- TP1/task.py
+++ TP1/task.py
@@ -57,3 +57,22 @@ for key, value in docs.items():
 
 for key, value in normalized_docs.items():
     print(key + ":", value)
+
+
+
+
+
+
+
+#     line 36
+# .strip(".,!?") only works for the start/end of a word
+# it fails if punctuation is in the middle, example : "don't" or "U.S.A."
+
+#     line 36
+# Use string.punctuation bcz it's better than hardcoding marks manually
+
+#     line 39
+# btw, you can use .get() here to handle the dict lookup and the "else" in one line
+
+#     line 46
+# you can use a dict comprehension to normalize everything in one go
